4bae1d0a2c2ae0e27015b519ba58d0aad42f9463
chongyoonkah <dcyk_87@hotmail.com>
2016-05-11 14:52:11 +0800

    latest

---
+++ AA_EyeSee/UnidentifiedValueViewController.swift
@@ -0,0 +1,35 @@
+//
+//  UnidentifiedValueViewController.swift
+//  AA_EyeSee
+//
+//  Created by Joanne Lim on 11/5/16.
+//  Copyright © 2016 abdoassem. All rights reserved.
+//
+
+import UIKit
+
+class UnidentifiedValueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
