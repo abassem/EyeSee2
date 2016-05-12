001011ddd72a778243d30b96da7eeb8f5fd0ccc4
Faris <hewrin@hotmail.com>
2016-05-12 13:59:12 +0800

    add image converter function

--- OpenCVWrapper.h
+++ OpenCVWrapper.h
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <CoreGraphics/CoreGraphics.h>
 #import <opencv2/highgui/highgui_c.h>
 @class CvVideoCamera;
 
--- OpenCVWrapper.mm
+++ OpenCVWrapper.mm
@@ -8,6 +8,9 @@
 
 #import "OpenCVWrapper.h"
 #import <opencv2/videoio/cap_ios.h>
+
+
+
 @interface OpenCVWrapper() <CvVideoCameraDelegate>
 @property CvVideoCamera *videoCamera;
 @end
@@ -30,10 +33,9 @@
         self.videoCamera.defaultAVCaptureVideoOrientation =
         AVCaptureVideoOrientationPortrait;
         self.videoCamera.defaultFPS = 30;
-        
-//        self.videoCamera.
     }
     [self.videoCamera start];
+    
 }
 
 - (void)stopCamera{
@@ -41,11 +43,46 @@
 }
 
 - (void)processImage:(cv::Mat &)image{
-    
+    UIImage *capturedImage = [self UIImageFromCVMat:image];
+
 }
 
-//+ (void)startVideoCamera{
-//    self->videoCamera = [[CvVideoCamera alloc] init];
-//}
+-(UIImage *)UIImageFromCVMat:(cv::Mat)cvMat
+{
+    NSData *data = [NSData dataWithBytes:cvMat.data length:cvMat.elemSize()*cvMat.total()];
+    CGColorSpaceRef colorSpace;
+    
+    if (cvMat.elemSize() == 1) {
+        colorSpace = CGColorSpaceCreateDeviceGray();
+    } else {
+        colorSpace = CGColorSpaceCreateDeviceRGB();
+    }
+    
+    CGDataProviderRef provider = CGDataProviderCreateWithCFData((__bridge CFDataRef)data);
+    
+    // Creating CGImage from cv::Mat
+    CGImageRef imageRef = CGImageCreate(cvMat.cols,                                 //width
+                                        cvMat.rows,                                 //height
+                                        8,                                          //bits per component
+                                        8 * cvMat.elemSize(),                       //bits per pixel
+                                        cvMat.step[0],                            //bytesPerRow
+                                        colorSpace,                                 //colorspace
+                                        kCGImageAlphaNone|kCGBitmapByteOrderDefault,// bitmap info
+                                        provider,                                   //CGDataProviderRef
+                                        NULL,                                       //decode
+                                        false,                                      //should interpolate
+                                        kCGRenderingIntentDefault                   //intent
+                                        );
+    
+    
+    // Getting UIImage from CGImage
+    UIImage *finalImage = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(colorSpace);
+    
+    return finalImage;
+}
+
 
 @end
