477e6752a8f3b7b4f02cb37999fffdee1ca0d0d8
chongyoonkah <dcyk_87@hotmail.com>
2016-05-18 10:49:38 +0800

    core data updated

---
+++ Wallet.swift
@@ -0,0 +1,17 @@
+//
+//  Wallet.swift
+//  bit
+//
+//  Created by Joanne Lim on 18/5/16.
+//  Copyright © 2016 abdoassem. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Wallet)
+class Wallet: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
